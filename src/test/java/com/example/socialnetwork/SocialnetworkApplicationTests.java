a8c4fcabd02e115104ad40b152afdaf89759fbfa
Gabriel Oliveira <gabrielgbo00@gmail.com>
2025-10-13 19:28:49 -0300

    Ponto de restauração antes do teste de projeto mínimo

--- src/test/java/com/example/socialnetwork/SocialnetworkApplicationTests.java
+++ src/test/java/com/example/socialnetwork/SocialnetworkApplicationTests.java
@@ -2,9 +2,25 @@ package com.example.socialnetwork;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class SocialnetworkApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", postgresqlContainer::getUsername);
+		registry.add("spring.datasource.password", postgresqlContainer::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
